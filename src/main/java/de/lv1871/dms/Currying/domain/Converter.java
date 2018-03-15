4a241ead7568fbf53d865d07bec83abc0726c201
David Ibl <david.ibl@xnoname.com>
2018-03-15 22:29:19 +0100

    solution Value Converter

--- src/main/java/de/lv1871/dms/Currying/domain/Converter.java
+++ src/main/java/de/lv1871/dms/Currying/domain/Converter.java
@@ -1,5 +1,8 @@
 package de.lv1871.dms.Currying.domain;
 
+import de.lv1871.dms.Currying.ExtendedFunction;
+
 public class Converter {
 
+	public static ExtendedFunction<Double, Double, Double> CONVERT = (a, b) -> a * b;
 }
--- src/test/java/de/lv1871/dms/Currying/ValueConverterTest.java
+++ src/test/java/de/lv1871/dms/Currying/ValueConverterTest.java
@@ -4,20 +4,27 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.function.Function;
 
+import org.junit.Test;
+
+import de.lv1871.dms.Currying.domain.Converter;
+
 public class ValueConverterTest {
 
 	/**
 	 * Implementiert einen Converter der auf Basis eines Werts die Neuberechnung
 	 * durchführt. Verwendet hierfür Currying.
 	 */
+	@Test
 	public void testValueConvertingEuroDollar() {
-		Function<Double, Double> euroDollarConverter = null; // Converter.base(2.51);
+		Function<Double, Double> euroDollarConverter = Converter.CONVERT.curry().apply(2.51); // Converter.base(2.51);
 
 		assertEquals(new Double(5.02), euroDollarConverter.apply(2.0));
 	}
 
+	@Test
 	public void testValueConvertingDollarEuro() {
-		Function<Double, Double> dollarEuroConverter = null; // Converter.base(0.76);
+		Function<Double, Double> dollarEuroConverter = Converter.CONVERT.curry().apply(0.76);
+		; // Converter.base(0.76);
 
 		assertEquals(new Double(1.52), dollarEuroConverter.apply(2.0));
 	}
